17091e0b6e5bbd4d2cf6c6bb891b28f5e8f0d719
palloncino <antonio.guiotto.dev@gmail.com>
2024-02-15 16:10:37 +0100

    resized to fit frame

--- src/components/Animation-01.tsx
+++ src/components/Animation-01.tsx
@@ -1,11 +1,54 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 
-const Animation01: FC<any> = ({}) => {
+const EclipseAnimation: FC<any> = () => {
 
-  return (
-    <span>Animation Here</span>
-  )
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  const importAndExecuteThreeJs = async () => {
+    const THREE = await import('three');
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+    camera.position.z = 5;
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xffffff, 1);
+
+    if (mountRef.current) {
+      mountRef.current.appendChild(renderer.domElement);
+    }
+
+    const updateSize = () => {
+      const previewMedia = document.querySelector('.preview-media');
+      if (previewMedia && mountRef.current) {
+        const { width, height } = previewMedia.getBoundingClientRect();
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+      }
+    };
+
+    updateSize();
+
+    window.addEventListener('resize', updateSize);
+
+
+    // rest of the code
+
+
+    return () => {
+      if (mountRef.current) {
+        mountRef.current.removeChild(renderer.domElement);
+      }
+      window.removeEventListener('resize', updateSize);
+    };
+  }
+
+  useEffect(() => {
+    importAndExecuteThreeJs();
+  }, []);
+
+  return <div id="EclipseAnimation" className="EclipseAnimation" ref={mountRef} />;
 
 }
 
-export default Animation01;
+export default EclipseAnimation;
